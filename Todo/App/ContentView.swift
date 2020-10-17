f06623404c3a5bbffe325f8f2859471094cb204b
Önder Koşar <onderkosar@hotmail.com>
2020-10-17 23:35:47 +0300

    add view is created

---
+++ Todo/App/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Önder Koşar on 17.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+    @State private var showingAddTodoView: Bool = false
+    
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            List(0 ..< 5) { item in
+                Text("Hello, world!")
+            } //: LIST
+            .navigationBarTitle("Todo", displayMode: .inline)
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingAddTodoView.toggle()
+                }) {
+                    Image(systemName: "plus")
+                } // ADD BUTTON
+                    .sheet(isPresented: $showingAddTodoView) {
+                        AddTodoView()
+                    }
+            )
+        } //: NAVIGATION VIEW
+    }
+}
+
+// MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
